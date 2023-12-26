ee7c3bd40090dfc27cef9ebfc61d33d3a4372f7c
Yorkie Makoto <yorkiefixer@gmail.com>
2023-12-26 17:07:32 +0800

    add protoc setup

--- build/build-protofiles.cjs
+++ build/build-protofiles.cjs
@@ -3,12 +3,16 @@ const os = require('node:os');
 const path = require('node:path');
 
 const isWindows = os.platform() === 'win32';
-const protocCommand = path.join(
+let protocCommand = path.join(
   __dirname,
   '../thirdparty/libs',
   isWindows ? 'Windows/x86_64' : 'Darwin',
   'bin/protoc');
-console.info(`    platform: "${os.platform()}"`);
+if (process.env.USE_SYSTEM_PROTOC === 'yes') {
+  protocCommand = 'protoc';
+}
+
+console.info(`     platform: "${os.platform()}"`);
 console.info(`      dirname: "${__dirname}"`);
 console.info(`protocCommand: "${protocCommand}"`);
 
