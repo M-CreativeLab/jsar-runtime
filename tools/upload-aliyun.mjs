a0d92caab20b259542b3bcdcd8c1827f59c7b551
Yorkie Makoto <yorkiefixer@gmail.com>
2024-01-13 02:22:19 +0800

    add aliyun upload tool

---
+++ tools/upload-aliyun.mjs
@@ -0,0 +1,33 @@
+import path from 'path';
+import fs from 'fs/promises';
+import OSS from 'ali-oss';
+
+const args = process.argv.slice(2);
+const [ version, filename ] = args;
+
+if (!version || !version.startsWith('amidala-v')) {
+  throw new Error('a valid version must be "amidala-v*", such as "amidala-v1.0.0"')
+}
+if (!filename) {
+  throw new Error('a valid filename must be provided')
+}
+if (fs.existsSync(filename)) {
+  throw new Error(`file ${filename} not found`)
+}
+
+const store = new OSS({
+  region: 'oss-cn-hangzhou',
+  accessKeyId: process.env.ALIYUN_ACCESS_KEY_ID,
+  accessKeySecret: process.env.ALIYUN_ACCESS_KEY_SECRET,
+  bucket: process.env.ALIYUN_BUCKET,
+});
+
+const ossFilename = `${version}/${filename}`;
+const artifactFilename = path.join(__dirname, '../', filename);
+
+console.info(`uploading ${artifactFilename} to ${ossFilename}...`);
+const resp = await store.put(
+  `web-assets/yodaos-jsar/runtime-artifacts/${ossFilename}`,
+  artifactFilename
+);
+console.info(resp);
