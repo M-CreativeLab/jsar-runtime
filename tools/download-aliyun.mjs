90dc32160560910dfa880682b0b0f33adfb4d62e
Yorkie Makoto <yorkiefixer@gmail.com>
2024-01-13 03:22:23 +0800

    workflow: add download artifacts for Release

---
+++ tools/download-aliyun.mjs
@@ -0,0 +1,31 @@
+import path from 'path';
+import OSS from 'ali-oss';
+
+const args = process.argv.slice(2);
+const [ version, filename ] = args;
+
+if (!version || !version.startsWith('amidala-v')) {
+  throw new Error('a valid version must be "amidala-v*", such as "amidala-v1.0.0"')
+}
+if (!filename) {
+  throw new Error('a valid filename must be provided')
+}
+
+const ossFilename = path.join(version, filename);
+const artifactFilename = new URL(path.join('../', filename), import.meta.url);
+console.log(`ossFilename: ${ossFilename}`);
+console.log(`artifactFilename: ${artifactFilename}`);
+
+const store = new OSS({
+  region: 'oss-cn-hangzhou',
+  accessKeyId: process.env.ALIYUN_ACCESS_KEY_ID,
+  accessKeySecret: process.env.ALIYUN_ACCESS_KEY_SECRET,
+  bucket: process.env.ALIYUN_BUCKET,
+});
+
+console.info(`downloading ${ossFilename}...`);
+const resp = await store.get(
+  `web-assets/yodaos-jsar/runtime-artifacts/${ossFilename}`,
+  artifactFilename.href
+);
+console.info(resp);
