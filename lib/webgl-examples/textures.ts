05726cd8ece04b4aeec2695025d013c59018048e
Yorkie Makoto <yorkiefixer@gmail.com>
2024-02-07 20:56:55 +0800

    add lighting

--- lib/webgl-examples/textures.ts
+++ lib/webgl-examples/textures.ts
@@ -5,25 +5,41 @@ import { requestRendererReady, requestAnimationFrame } from '../bindings/rendere
 
 const vertexSource = `
 attribute vec4 aVertexPosition;
+attribute vec3 aVertexNormal;
 attribute vec2 aTextureCoord;
 
+uniform mat4 uNormalMatrix;
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 
 varying highp vec2 vTextureCoord;
+varying highp vec3 vLighting;
 
 void main(void) {
   gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
   vTextureCoord = aTextureCoord;
+
+  // Apply lighting effect
+
+  highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+  highp vec3 directionalLightColor = vec3(1, 1, 1);
+  highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+
+  highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+
+  highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+  vLighting = ambientLight + (directionalLightColor * directional);
 }
   `;
 
 const fragmentSource = `
 varying highp vec2 vTextureCoord;
+varying highp vec3 vLighting;
 uniform sampler2D uSampler;
 
 void main(void) {
-  gl_FragColor = texture2D(uSampler, vTextureCoord);
+  highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
+  gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
 }
   `;
 
@@ -51,6 +67,7 @@ export default function run() {
       program,
       attribLocations: {
         vertexPosition: gl.getAttribLocation(program, "aVertexPosition"),
+        vertexNormal: gl.getAttribLocation(program, "aVertexNormal"),
         textureCoord: gl.getAttribLocation(program, "aTextureCoord"),
       },
       uniformLocations: {
@@ -59,6 +76,7 @@ export default function run() {
           "uProjectionMatrix"
         ),
         modelViewMatrix: gl.getUniformLocation(program, "uModelViewMatrix"),
+        normalMatrix: gl.getUniformLocation(program, "uNormalMatrix"),
         uSampler: gl.getUniformLocation(program, "uSampler"),
       },
     };
@@ -88,11 +106,13 @@ export default function run() {
 
 function initBuffers(gl) {
   const positionBuffer = initPositionBuffer(gl);
+  const normalBuffer = initNormalBuffer(gl);
   const textureCoordBuffer = initTextureBuffer(gl);
   const indexBuffer = initIndexBuffer(gl);
 
   return {
     position: positionBuffer,
+    normal: normalBuffer,
     textureCoord: textureCoordBuffer,
     indices: indexBuffer,
   };
@@ -197,6 +217,39 @@ function initPositionBuffer(gl: WebGLRenderingContext) {
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   return positionBuffer;
+}
+
+function initNormalBuffer(gl: WebGLRenderingContext) {
+  const normalBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+
+  const vertexNormals = [
+    // Front
+    0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+
+    // Back
+    0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0,
+
+    // Top
+    0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,
+
+    // Bottom
+    0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0,
+
+    // Right
+    1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+
+    // Left
+    -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0,
+  ];
+
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(vertexNormals),
+    gl.STATIC_DRAW
+  );
+
+  return normalBuffer;
 }
 
 function initColorBuffer(gl: WebGLRenderingContext) {
@@ -370,6 +423,10 @@ function drawScene(gl: WebGLRenderingContext, programInfo, buffers, texture, cub
     [1, 0, 0]
   ); // axis to rotate around (X)
 
+  const normalMatrix = mat4.create();
+  mat4.invert(normalMatrix, modelViewMatrix);
+  mat4.transpose(normalMatrix, normalMatrix);
+
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
   setPositionAttribute(gl, buffers, programInfo);
@@ -378,6 +435,8 @@ function drawScene(gl: WebGLRenderingContext, programInfo, buffers, texture, cub
 
   // Tell WebGL which indices to use to index the vertices
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+
+  setNormalAttribute(gl, buffers, programInfo);
 
   // Tell WebGL to use our program when drawing
   gl.useProgram(programInfo.program);
@@ -392,6 +451,11 @@ function drawScene(gl: WebGLRenderingContext, programInfo, buffers, texture, cub
     programInfo.uniformLocations.modelViewMatrix,
     false,
     modelViewMatrix
+  );
+  gl.uniformMatrix4fv(
+    programInfo.uniformLocations.normalMatrix,
+    false,
+    normalMatrix
   );
 
   // Tell WebGL we want to affect texture unit 0
@@ -469,4 +533,24 @@ function setTextureAttribute(gl: WebGLRenderingContext, buffers, programInfo) {
     offset
   );
   gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
+}
+
+// Tell WebGL how to pull out the normals from
+// the normal buffer into the vertexNormal attribute.
+function setNormalAttribute(gl, buffers, programInfo) {
+  const numComponents = 3;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  const offset = 0;
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.normal);
+  gl.vertexAttribPointer(
+    programInfo.attribLocations.vertexNormal,
+    numComponents,
+    type,
+    normalize,
+    stride,
+    offset
+  );
+  gl.enableVertexAttribArray(programInfo.attribLocations.vertexNormal);
 }
