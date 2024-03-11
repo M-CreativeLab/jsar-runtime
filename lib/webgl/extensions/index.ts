91f2483e4fe5695ee3a8d1cfc9bec939b3bfdf3e
Yorkie Makoto <yorkiefixer@gmail.com>
2024-03-11 15:03:49 +0800

    gl: add support for oes_texture_float_linear

--- lib/webgl/extensions/index.ts
+++ lib/webgl/extensions/index.ts
@@ -1,6 +1,7 @@
 import OES_element_index_uint from './oes_element_index_uint';
 import OES_standard_derivatives from './oes_standard_derivatives';
 import OES_texture_float from './oes_texture_float';
+import OES_texture_float_linear from './oes_texture_float_linear';
 
 export function getExtension(gl: WebGLRenderingContext, name: string) {
   if (name === 'OES_element_index_uint') {
@@ -9,6 +10,9 @@ export function getExtension(gl: WebGLRenderingContext, name: string) {
     return new OES_standard_derivatives();
   } else if (name === 'OES_texture_float') {
     return new OES_texture_float();
+  } else if (name === 'OES_texture_float_linear') {
+    return new OES_texture_float_linear();
+  } else {
+    return null;
   }
-  return null;
 }
