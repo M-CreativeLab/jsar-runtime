1166ba615f094e866cf90ab244f84a5ed1b02417
Yorkie Makoto <yorkiefixer@gmail.com>
2024-05-15 22:23:36 +0800

    fixup

--- src/bindings/canvas/binding.cpp
+++ src/bindings/canvas/binding.cpp
@@ -1,8 +1,10 @@
 #include "binding.hpp"
 #include "canvas.hpp"
+#include "rendering_context2d.hpp"
 
 Napi::Object InitCanvasModule(Napi::Env env, Napi::Object exports)
 {
   canvas::OffscreenCanvas::Init(env, exports);
+  canvas::CanvasRenderingContext2D::Init(env, exports);
   return exports;
 }
