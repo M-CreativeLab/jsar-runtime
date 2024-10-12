e71e46ca0922607e3609a69a06eb9c2707f38b13
Yorkie Makoto <yorkiefixer@gmail.com>
2024-10-12 12:36:09 +0800

    builtin_scene: add material

--- src/client/builtin_scene/renderable_object.hpp
+++ src/client/builtin_scene/renderable_object.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include "./material_base.hpp"
+
 namespace builtin_scene
 {
   class IRenderableObject
   {
   public:
     virtual ~IRenderableObject() = default;
+
+  public:
+    Material material;
   };
 
   class Transform : public IRenderableObject
