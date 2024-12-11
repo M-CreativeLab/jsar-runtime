3e2f6eb0dd6ad4ade4c57a1b7959efd6a78693e6
Yorkie Makoto <yorkiefixer@gmail.com>
2024-12-11 15:40:54 +0800

    Fix(runtime): install client executable at runtime to fix android system app issue
    The dladdr() fails in the android system app, this patch embed the client binary and related libs into
    the TransmuteCore library, and install to the data path at runtime to use.

--- crates/jsbundle/lib.rs
+++ crates/jsbundle/lib.rs
@@ -2,6 +2,8 @@ use flate2::read::GzDecoder;
 use lazy_static::lazy_static;
 use std::io::Read;
 
+const LIBNODE_SRC: &[u8] = include_bytes!("res/libnode.lib");
+const LIBNODE_SRC_MD5: &str = include_str!("res/libnode.lib.md5");
 const JSBOOTSTRAP_BABYLON_COMPRESSED: &[u8] = include_bytes!("jsar-bootstrap-babylon.js.gz");
 const JSBUNDLE_CLIENT_ENTRY_COMPRESSED: &[u8] = include_bytes!("jsar-client-entry.js.gz");
 const JSBUNDLE_WEBWORKERS_ENTRY_COMPRESSED: &[u8] = include_bytes!("jsar-webworkers-entry.js.gz");
@@ -17,6 +19,26 @@ lazy_static! {
   static ref JSBOOTSTRAP_BABYLON_SRC: String = decompress_js_source(JSBOOTSTRAP_BABYLON_COMPRESSED);
   static ref JSBUNDLE_CLIENT_ENTRY_SRC: String = decompress_js_source(JSBUNDLE_CLIENT_ENTRY_COMPRESSED);
   static ref JSBUNDLE_WEBWORKERS_ENTRY_SRC: String = decompress_js_source(JSBUNDLE_WEBWORKERS_ENTRY_COMPRESSED);
+}
+
+#[no_mangle]
+extern "C" fn get_libnode_ptr() -> *const u8 {
+  LIBNODE_SRC.as_ptr()
+}
+
+#[no_mangle]
+extern "C" fn get_libnode_size() -> usize {
+  LIBNODE_SRC.len()
+}
+
+#[no_mangle]
+extern "C" fn get_libnode_md5_ptr() -> *const u8 {
+  LIBNODE_SRC_MD5.as_ptr()
+}
+
+#[no_mangle]
+extern "C" fn get_libnode_md5_size() -> usize {
+  LIBNODE_SRC_MD5.len()
 }
 
 #[no_mangle]
