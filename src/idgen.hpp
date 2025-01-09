f5c591dc34f0c13df3cd6a34f409d55b229661ff
Yorkie Makoto <yorkiefixer@gmail.com>
2025-01-09 13:47:39 +0800

    idgen: optimize

--- src/idgen.hpp
+++ src/idgen.hpp
@@ -2,36 +2,43 @@
 
 #include <cstdint>
 #include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <type_traits>
 
 template <typename IdType>
 class TrIdTraits
 {
 public:
-  static const IdType MaxId = UINT32_MAX;
+  static constexpr IdType MaxId = std::numeric_limits<IdType>::max();
 };
+
 template <>
 class TrIdTraits<uint64_t>
 {
 public:
-  static const uint64_t MaxId = UINT64_MAX;
+  static constexpr uint64_t MaxId = std::numeric_limits<uint64_t>::max();
 };
+
 template <>
 class TrIdTraits<uint32_t>
 {
 public:
-  static const uint32_t MaxId = UINT32_MAX;
+  static constexpr uint32_t MaxId = std::numeric_limits<uint32_t>::max();
 };
+
 template <>
 class TrIdTraits<uint16_t>
 {
 public:
-  static const uint16_t MaxId = UINT16_MAX;
+  static constexpr uint16_t MaxId = std::numeric_limits<uint16_t>::max();
 };
+
 template <>
 class TrIdTraits<uint8_t>
 {
 public:
-  static const uint8_t MaxId = UINT8_MAX;
+  static constexpr uint8_t MaxId = std::numeric_limits<uint8_t>::max();
 };
 
 /**
@@ -51,14 +58,14 @@ public:
    * Create a new Id generator.
    *
    * @param initialId The initial id to start with, and the default value is 0.
-   * @param maxId The maximum id value, and the default value is `UINT32_MAX`.
+   * @param maxId The maximum id value, and the default value is `TrIdTraits<IdType>::MaxId`.
    */
   TrIdGeneratorBase(IdType initialId = 0, IdType maxId = TrIdTraits<IdType>::MaxId)
-      : counter(initialId),
-        initialId(initialId),
-        maxId(maxId)
+      : counter_(initialId),
+        initialId_(initialId),
+        maxId_(maxId)
   {
-    if (maxId < initialId)
+    if (maxId_ < initialId_)
       throw std::runtime_error("The maximum id value should be greater than the initial id value.");
   }
   ~TrIdGeneratorBase() = default;
@@ -68,25 +75,27 @@ public:
    * @returns a `IdType` value that can be used as the id value, when the next possible id is reached to the maximum value, it
    *          will be reset to the initial id.
    */
-  IdType get()
+  [[nodiscard]] IdType get() noexcept
   {
-    if (counter >= maxId)
-      counter = initialId;
-    return counter++;
+    if (counter_ >= maxId_)
+      counter_ = initialId_;
+    return counter_++;
   }
+
   /**
    * @returns a `IdType` value that represents the minimum id value, namely the `initialId`.
    */
-  IdType min() const { return initialId; }
+  [[nodiscard]] IdType min() const noexcept { return initialId_; }
+
   /**
-   * @returns a `uint32_t` value that represents the maximum id value, namely the `maxId`.
+   * @returns a `IdType` value that represents the maximum id value, namely the `maxId`.
    */
-  IdType max() const { return maxId; }
+  [[nodiscard]] IdType max() const noexcept { return maxId_; }
 
 private:
-  IdType counter;
-  IdType initialId;
-  IdType maxId;
+  IdType counter_;    // Current counter value
+  IdType initialId_;  // Initial ID value
+  IdType maxId_;      // Maximum ID value
 };
 
 using TrIdGenerator = TrIdGeneratorBase<uint32_t>;
