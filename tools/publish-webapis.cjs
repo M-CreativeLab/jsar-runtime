95eabc3247672d07dc2cf550b96530c6c633a895
Yorkie Liu <yorkiefixer@gmail.com>
2025-06-15 23:35:44 +0800

    build: fix workflow file

--- tools/publish-webapis.cjs
+++ tools/publish-webapis.cjs
@@ -73,7 +73,7 @@ To use the types provided by this package, configure the types package in your p
 const readmePath = path.join(webApisDir, 'README.md');
 fs.writeFileSync(readmePath, readmeContent);
 
-execSync('npm publish', { 
+execSync('npm publish --tag ' + (isRelease ? 'latest' : 'daily'), { 
   cwd: webApisDir, 
   stdio: 'inherit',
   env: { ...process.env }  // Pass all environment variables including NODE_AUTH_TOKEN
