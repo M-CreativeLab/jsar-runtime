0c3e19e9d808a7d6a94ee95eeebbb282dcf7eaf8
Yorkie Makoto <yorkiefixer@gmail.com>
2025-10-23 14:33:22 +0800

    update

--- types/nodejs-patch.d.ts
+++ types/nodejs-patch.d.ts
@@ -4,12 +4,6 @@
 declare namespace NodeJS {
   interface Process {
     /**
-     * It loads the transmute's native browser module.
-     */
-    _linkedBinding(module: 'transmute:browser'): {
-      // The browser APIs
-    };
-    /**
      * It loads the transmute's native DOM module, and returns some internal classes for host.
      */
     _linkedBinding(module: 'transmute:dom'): {
@@ -29,14 +23,10 @@ declare namespace NodeJS {
     _linkedBinding(module: 'transmute:messaging'): {
       NativeEventTarget: typeof Transmute.NativeEventTarget;
     };
-    _linkedBinding(module: 'transmute:math3d'): {
-      matrixMultiplyToArray: (a: number[], b: number[], out: number[], offset: number) => void;
-    };
     _linkedBinding(module: 'transmute:renderer'): {
       RenderLoop: typeof Transmute.RenderLoop;
       AnimationFrameListener: typeof Transmute.AnimationFrameListener;
     };
-    _linkedBinding(module: 'transmute:cssom'): {};
 
     /**
      * Loads a native linked binding.
